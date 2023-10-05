35721d265d70fe6f50eac74f499828c52024c212
Rai Saraiva <rai.saraiva70@gmail.com>
2023-10-05 18:25:55 -0300

    Organizando o Repositorio

--- scripts.js
+++ scripts.js
@@ -5,9 +5,9 @@ const key = "988dd6a8364797e88e6269ffd234d050";
 function colocarDadosNaTela(dados) {
     document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
 
-    document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "°C"
+    document.querySelector(".temp").innerHTML = "Temperatura: " + Math.floor(dados.main.temp) + "°C"
     document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description
-    document.querySelector(".umidade").innerHTML = dados.main.humidity + "%" 
+    document.querySelector(".umidade").innerHTML = "Umidade: " + dados.main.humidity + "%" 
     document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
 }
 async function buscarCidade(cidade) {
